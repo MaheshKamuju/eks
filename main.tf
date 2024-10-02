2fe7ef5ff53d799c70feccef96844cabd43b6f2a
MaheshKamuju <54261609+MaheshKamuju@users.noreply.github.com>
2024-10-02 15:37:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,12 +15,7 @@ data "aws_availability_zones" "available" {
 }
 
 locals {
-  cluster_name = "devops-eks-${random_string.suffix.result}"
-}
-
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
+  cluster_name = "devops-eks-cluster"
 }
 
 module "vpc" {
